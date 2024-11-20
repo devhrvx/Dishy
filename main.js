1ecb868aeec7d3487c9278a3eade151027b4ef19
hrvyCased <salinas.johnharvey11@gmail.com>
2024-11-20 19:59:28 +0800

    Add initial project files with HTML, CSS, and Firebase authentication setup

---
+++ main.js
@@ -0,0 +1,42 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDvXage0SbYUMV8RFZzn48ANw4GX_D6Zfo",
+  authDomain: "dishy-280a7.firebaseapp.com",
+  projectId: "dishy-280a7",
+  storageBucket: "dishy-280a7.firebasestorage.app",
+  messagingSenderId: "785622443437",
+  appId: "1:785622443437:web:acbe9c5813fb60be0c2d24",
+  measurementId: "G-N3X5D4SQ58"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+auth.languageCode = 'en'
+const provider = new GoogleAuthProvider();
+
+const googleLogin = document.getElementById("loginBtn");
+googleLogin.addEventListener("click", function(){
+  signInWithPopup(auth, provider)
+  .then((result)=> {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const user = result.user;
+    console.log(user);
+    const displayName = user.displayName;
+    localStorage.setItem("displayName", displayName);
+    window.location.href = "welcome.html";
+  }).catch((error)=> {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(`Code: ${errorCode}\n${errorMessage}`);
+  })
+}); 
