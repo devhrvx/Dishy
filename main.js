d9a873bc86127471fad09a5568b05183554a77da
John Harvey Salinas <salinas.johnharvey11@gmail.com>
2024-11-20 18:16:08 -0800

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ googleLogin.addEventListener("click", function(){
     console.log(user);
     const displayName = user.displayName;
     localStorage.setItem("displayName", displayName);
-    window.location.href = "index.html";
+    window.location.href = "home.html";
   }).catch((error)=> {
     const errorCode = error.code;
     const errorMessage = error.message;
