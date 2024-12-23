856f7d1740ad0c51bc229cd8bf4d089398076122
devhrvx <salinas.johnharvey11@gmail.com>
2024-12-23 11:41:45 +0800

    Dynamically import fetch in verify-recaptcha.js to improve compatibility

--- api/verify-recaptcha.js
+++ api/verify-recaptcha.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { token } = req.body;
@@ -15,6 +13,9 @@ export default async function handler(req, res) {
         }
 
         try {
+            // Dynamically import fetch
+            const { default: fetch } = await import('node-fetch');
+
             const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
